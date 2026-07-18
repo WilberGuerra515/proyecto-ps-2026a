5fe59469543d97657757b5c1e3453fbd7e4a81bd
wilber <wguerra@unsa.edu.pe>
2026-07-18 18:57:55 -0500

    Crear e insertar pestañas vacías (placeholders) en el QTabWidget

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -1,4 +1,5 @@
 #include "MainWindow.h"
+#include <QWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent) {
@@ -6,11 +7,24 @@ MainWindow::MainWindow(QWidget *parent)
     setWindowTitle("Administrador de Sistemas C/C++");
     resize(1024, 768);
 
-    // Instanciar el QTabWidget
     tabWidget = new QTabWidget(this);
-    
-    // Establecer el QTabWidget como el núcleo de la ventana
     setCentralWidget(tabWidget);
+
+    // Crear widgets vacíos (lienzos en blanco) para cada módulo
+    QWidget *tabTasks = new QWidget();
+    QWidget *tabFiles = new QWidget();
+    QWidget *tabCommands = new QWidget();
+    QWidget *tabBackups = new QWidget();
+    QWidget *tabBash = new QWidget();
+    QWidget *tabDownloads = new QWidget();
+
+    // Insertar las pestañas en el QTabWidget con sus respectivos títulos
+    tabWidget->addTab(tabTasks, "Tareas");
+    tabWidget->addTab(tabFiles, "Archivos");
+    tabWidget->addTab(tabCommands, "Comandos");
+    tabWidget->addTab(tabBackups, "Respaldos");
+    tabWidget->addTab(tabBash, "Bash");
+    tabWidget->addTab(tabDownloads, "Descargas");
 }
 
 MainWindow::~MainWindow() {
