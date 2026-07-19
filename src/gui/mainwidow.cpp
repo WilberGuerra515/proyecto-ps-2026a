05e82bcb6dc1ba59c953cfce1e7f100e6bdbb763
DiegoSantaC <dsantacruzv@unsa.edu.pe>
2026-07-19 18:09:06 -0500

    Comienzo de desarrollo de modulos: Generando interfaz base para los modulos del sistema

--- src/gui/mainwidow.cpp
+++ src/gui/mainwidow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "process_manager.h"
 
 #include "backend.h"
 #include <QDebug>
@@ -19,14 +20,4 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_btnProcess_clicked()
 {
-    int num1 = ui->spinNum1->value();
-    int num2 = ui->spinNum2->value();
-    
-    int operacionSeleccionada = ui->comboOp->currentIndex();
-    
-    int resultado = process_data(num1, num2, operacionSeleccionada);
-    
-    ui->lblResult->setText("Resultado del backend (C): " + QString::number(resultado));
-    
-    qDebug() << "Operación enviada al Backend (C):" << operacionSeleccionada << "| Resultado:" << resultado;
 }
