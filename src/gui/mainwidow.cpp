76c457d4aabd4ab2e8b7343317998bbbe3b832b8
wilber <wguerra@unsa.edu.pe>
2026-07-19 22:31:29 -0500

    Conectando  interfaz Qt con backend de procesos

--- src/gui/mainwidow.cpp
+++ src/gui/mainwidow.cpp
@@ -40,3 +40,55 @@ void MainWindow::setupComponentsView()
     connect(ui->btnCont, &QPushButton::clicked, this, &MainWindow::handleProcessSignal);
 
 }
+
+
+// LÓGICA MÓDULO 1
+
+void MainWindow::refreshProcesses()
+{
+    ui->processTreeWidget->clear();
+    int count = 0;
+    CError error;
+    
+    ProcessInfo* list = get_process_list(&count, &error);
+    if (!list) {
+        QMessageBox::warning(this, "Error de Lectura", QString::fromUtf8(error.message));
+        return;
+    }
+
+    for (int i = 0; i < count; ++i) {
+        QStringList rowData;
+        rowData << QString::number(list[i].pid)
+                << QString::number(list[i].ppid)
+                << QString::fromUtf8(list[i].name)
+                << QString("%1 KB").arg(list[i].memory_rss);
+
+        new QTreeWidgetItem(ui->processTreeWidget, rowData);
+    }
+    free_c_pointer(list);
+}
+
+void MainWindow::handleProcessSignal()
+{
+    QTreeWidgetItem *selectedItem = ui->processTreeWidget->currentItem();
+    if (!selectedItem) {
+        QMessageBox::information(this, "Control", "Selecciona un proceso de la lista.");
+        return;
+    }
+
+    int pid = selectedItem->text(0).toInt();
+    QPushButton *clickedButton = qobject_cast<QPushButton*>(sender());
+    
+    int target_signal = SIGKILL;
+    if (clickedButton == ui->btnStop) target_signal = SIGSTOP;
+    else if (clickedButton == ui->btnCont) target_signal = SIGCONT;
+
+    CError error;
+    if (send_signal_to_process(pid, target_signal, &error) == 0) {
+        QMessageBox::information(this, "Éxito", QString("Señal enviada al PID %1.").arg(pid));
+        refreshProcesses();
+    } else {
+        QMessageBox::critical(this, "Error", QString::fromUtf8(error.message));
+    }
+}
+
